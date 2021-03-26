46327b11e2c00f6640e8896e774fc3253682588a
pranavanrao <pranavanrao@gmail.com>
2021-03-26 20:31:18 +0530

    Added custom static side navigation bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import Sidebar from './components/sidebar/index'
+// import { Editor } from 'react-draft-wysiwyg';
+// import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import Navbar from './components/sidebar/Sidebar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Sidebar></Sidebar>
-        <Editor
-          // editorState={editorState}
-          // toolbarClassName="toolbarClassName"
-          // wrapperClassName="wrapperClassName"
-          // editorClassName="editorClassName"
-          // onEditorStateChange={this.onEditorStateChange}
-        />
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/reports' component={Reports} />
+            <Route path='/products' component={Products} />
+          </Switch>
+        </Router>
+        {/* <Editor
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={this.onEditorStateChange}
+        /> */}
       </header>
     </div>
   );
