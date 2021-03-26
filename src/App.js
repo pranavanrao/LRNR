8edbb90fc215ed75540734f5e1409bda8a351e39
pranavanrao <pranavanrao@gmail.com>
2021-03-26 19:24:23 +0530

    Added Wysiwyg editor and side navigation bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import Sidebar from './components/sidebar/index'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Sidebar></Sidebar>
+        <Editor
+          // editorState={editorState}
+          // toolbarClassName="toolbarClassName"
+          // wrapperClassName="wrapperClassName"
+          // editorClassName="editorClassName"
+          // onEditorStateChange={this.onEditorStateChange}
+        />
       </header>
     </div>
   );
